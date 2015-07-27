174225596be4626a1e7b1a0283e50dec316669ef
Patrick Pachur <patrick.pachur@gmail.com>
2015-07-27 10:13:16 +0200

    some more data

--- app/components/Node.jsx
+++ app/components/Node.jsx
@@ -5,7 +5,7 @@ export default class Node extends React.Component {
     // console.log(this.props);
     return(
       <div className="Node">
-        {this.props.data.id}: {this.props.data.addr}
+        {this.props.data.id}: {this.props.data.addr}, up: {(this.props.data.bytessent /1024).toFixed(2)} kB, down: {(this.props.data.bytesrecv /1024).toFixed(2)} kB
       </div>
     );
   }}
